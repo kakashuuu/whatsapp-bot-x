326ba08cc315cfc9708b1ba0afb2e75ce0bbfb13
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-23 16:57:25 +0530

    Update Ping.js

--- src/Commands/Moderation/Ping.js
+++ src/Commands/Moderation/Ping.js
@@ -1,14 +1,15 @@
-import { Command, BaseCommand, Message } from '../../Structures'
-import { IArgs } from '../../Types'
+const Command = require('../../Structures/Command')
+const Message = require('../../Structures/Message')
 
 module.exports = class command extends Command {
     constructor() {
-        super('toggle', {
-            description: 'Toggles a command state',
-            usage: 'toggle --command=[command_name] --state=[disable/enable] | <reason_for_disabling_the_command>',
-            exp: 10,
-            category: 'dev',
-            cooldown: 10
+        super('ping', {
+            description: 'Tags all of the members in a group',
+            usage: 'ping (--tags=hidden)',
+            category: 'moderation',
+            exp: 35,
+            cooldown: 15,
+            aliases: ['all', 'tagall', 'everyone']
         })
     }
 
@@ -18,24 +19,25 @@ module.exports = class command extends Command {
      * @returns {Promise<void>}
      */
 
-        if (!M.groupMetadata) return void M.reply('*Try Again!*')
+    execute = async (M, { flags, context }) => {
+    if (!M.groupMetadata) return void M.reply('*Try Again!*')
         const hidden = this.getPingOptions(flags)
         flags.forEach((flag) => (context = context.replace(flag, '')))
         const message = context ? context.trim() : M.quoted ? M.quoted.content : ''
-        let text = `${message !== '' ? `🧧 *Message: ${message}*\n\n` : ''}🔰 *Group:* ${
+        let text = `${message !== '' ? `🏮 *Message: ${message}*\n\n` : ''}💬 *Group:* ${
             M.groupMetadata.subject
-        }\n🀄 *Members:* ${M.groupMetadata.participants.length}\n📢 *Init: @${
+        }\n👥 *Members:* ${M.groupMetadata.participants.length}\n📣 *Tagger: @${
             M.sender.jid.split('@')[0]
-        }*\n🌀 *Tags:* ${hidden ? '*[HIDDEN]*' : '\n'}`
-        const botJid = this.client.correctJid(this.client.user?.id || '')
+        }*\n📧 *Tags:* ${hidden ? '*[HIDDEN]*' : '\n'}`
+        const botJid = this.helper.correctJid(this.client.user?.id || '')
         if (!hidden) {
-            text += `\n🏮 *@${botJid.split('@')[0]}*`
-            const mods: string[] = []
-            const admins: string[] = []
-            const members: string[] = []
+            text += `\n🎉 *@${botJid.split('@')[0]}*`
+            const mods = []
+            const admins = []
+            const members = []
             for (const jid of M.groupMetadata.participants.map((x) => x.id)) {
                 if (jid === M.sender.jid || jid === botJid) continue
-                if (this.client.config.mods.includes(jid)) {
+                if (this.helper.config.mods.includes(jid)) {
                     mods.push(jid)
                     continue
                 }
@@ -46,9 +48,9 @@ module.exports = class command extends Command {
                 members.push(jid)
             }
             for (let i = 0; i < mods.length; i++) text += `${i === 0 ? '\n\n' : '\n'}🏅 *@${mods[i].split('@')[0]}*`
-            for (let i = 0; i < admins.length; i++) text += `${i === 0 ? '\n\n' : '\n'}🥇 *@${admins[i].split('@')[0]}*`
+            for (let i = 0; i < admins.length; i++) text += `${i === 0 ? '\n\n' : '\n'}👑 *@${admins[i].split('@')[0]}*`
             for (let i = 0; i < members.length; i++)
-                text += `${i === 0 ? '\n\n' : '\n'}🥈 *@${members[i].split('@')[0]}*`
+                text += `${i === 0 ? '\n\n' : '\n'}⭐ *@${members[i].split('@')[0]}*`
         }
         return void (await M.reply(
             text,
@@ -60,7 +62,12 @@ module.exports = class command extends Command {
         ))
     }
 
-    private getPingOptions = (flags: string[]): boolean => {
+    /**
+     * @param {string[]} flags 
+     * @returns {boolean}
+     */
+
+    getPingOptions = (flags) => {
         if (!flags.length) return false
         const taggingFlags = flags.filter((flag) => flag.startsWith('--tags='))
         let hidden = false
@@ -68,4 +75,3 @@ module.exports = class command extends Command {
         return hidden
     }
 }
-
