cb4b444afbed93feda54c1d550deb921ea0d2e9d
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-23 18:40:03 +0530

    Update Help.js

--- src/Commands/General/Help.js
+++ src/Commands/General/Help.js
@@ -26,12 +26,12 @@ module.exports = class command extends Command {
                 command,
                 data
             }))
-            let text = `👋🏻 (❤️ω💙)Konichiwa! *@${M.sender.jid.split('@')[0]}*, I'm ♡︎ᴀIᴋA♡︎
-              📒Note: This is just a simple bot without any fun commands soon all commands will be add in this bot!
-          
-              🏮My prefix is - ${this.helper.config.prefix}
-              ☘️ Owner ~ Toshi_sama
-              🧧Instagram - https://instagram.com/itz_toshi_02
+            let text = `👋🏻 (❤️ω💙)Konichiwa! *@${M.sender.jid.split('@')[0]}*, I'm ♡︎ᴀIᴋA♡︎!
+📒Note: This is just a simple bot without any fun commands soon all commands will be add in this bot!
+
+🏮My prefix is - ${this.helper.config.prefix}
+☘️ Owner ~ Toshi_sama
+🧧Instagram - https://instagram.com/itz_toshi_02
 
 *━━━❰ General ❱━━━*
 
@@ -63,6 +63,22 @@ ${this.helper.config.prefix}manga
 ${this.helper.config.prefix}anime`
 
 return void (await M.reply(text, 'text', undefined, undefined, undefined, [M.sender.jid]))
+} else {
+            const cmd = context.trim().toLowerCase()
+            const command = this.handler.commands.get(cmd) || this.handler.aliases.get(cmd)
+            if (!command) return void M.reply(`No command found | *"${context.trim()}"*`)
+            return void M.reply(
+                `🎐 *Command:* ${this.helper.utils.capitalize(command.name)}\n🎴 *Aliases:* ${
+                    !command.config.aliases
+                        ? ''
+                        : command.config.aliases.map((alias) => this.helper.utils.capitalize(alias)).join(', ')
+                }\n🔗 *Category:* ${this.helper.utils.capitalize(command.config.category)}\n⏰ *Cooldown:* ${
+                    command.config.cooldown ?? 3
+                }s\n🎗 *Usage:* ${command.config.usage
+                    .split('||')
+                    .map((usage) => `${this.helper.config.prefix}${usage.trim()}`)
+                    .join(' | ')}\n🧧 *Description:* ${command.config.description}`
+            )
         }
     }
 }
