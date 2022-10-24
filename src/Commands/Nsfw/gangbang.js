331f4272f169839e35a132bef19986e92b965810
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-24 19:00:36 +0530

    Create gangbang.js

---
+++ src/Commands/Nsfw/gangbang.js
@@ -0,0 +1,33 @@
+const Command = require('../../Structures/Command')
+
+module.exports = class command extends Command {
+    constructor() {
+        super('gangbang', {
+            description: 'Sends a random nsfw ero image',
+            category: 'nsfw',
+            usage: 'gangbang',
+            exp: 20,
+            cooldown: 5
+        })
+    }
+
+    /**
+     * @param {Message} M
+     * @returns {Promise<void>}
+     */
+
+    execute = async (M) => {
+       const prefix = this.helper.config.prefix
+       const buttons = [
+        { buttonId: `${this.helper.config.prefix}gangbang`, buttonText: {displayText: 'NEXT'}, type: 1},
+     ]
+       const buttonMessage = {
+       image: { url: 'https://api-reysekha.herokuapp.com/api/nsfw/gangbang?apikey=APIKEY'},
+       caption: '*Here you go!*',
+       footer: `Aika`,
+       buttons: buttons,
+       headerType: 1
+     }
+       return void (await this.client.sendMessage(M.from, buttonMessage))
+    }
+}
