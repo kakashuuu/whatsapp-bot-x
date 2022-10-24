07cec5f1bb6ab97b8dd97b97fb4725d0168a75f0
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-24 17:34:15 +0530

    Create blowjob.js

---
+++ src/Commands/Nsfw/blowjob.js
@@ -0,0 +1,33 @@
+const Message = require('../../Structures)
+
+module.exports = class command extends Command {
+    constructor() {
+        super('blowjob', {
+            description: 'Sends a random nsfw blowjob image',
+            category: 'nsfw',
+            usage: 'blowjob',
+            exp: 20,
+            cooldown: 5
+        })
+    }
+
+    /**
+     * @param {Message} M
+     * @returns {Promise<void>}
+     */
+
+    execute = async (M) => {
+       const prefix = this.client.config.prefix
+       const buttons = [
+        { buttonId: `${prefix}blowjob`, buttonText: {displayText: 'NEXT ➡️'}, type: 1},
+     ]
+       const buttonMessage = {
+       image: { url: 'https://api-reysekha.herokuapp.com/api/nsfw/blowjob?apikey=APIKEY'},
+       caption: '*Here, you go!*',
+       footer: `${this.client.config.name}`,
+       buttons: buttons,
+       headerType: 1
+     }
+       return void (await this.client.sendMessage(M.from, buttonMessage))
+    }
+}
