a6c71ef083dfa3abe30737241a8749b03a2c6ee5
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-25 00:21:41 +0530

    Update Bank.js

--- src/Commands/Economy/Bank.js
+++ src/Commands/Economy/Bank.js
@@ -1,1 +1,35 @@
-Hi
+const Command = require('../../Structures/Database')
+
+module.exports = class command extends Command {
+    constructor() {
+        super('bank', {
+            description: 'Sends the user bank',
+            category: 'economey',
+            usage: 'bank',
+            exp: 20,
+            cooldown: 5
+        })
+    }
+
+    /**
+     * @param {Message} M
+     * @param {import('../../Structures/Database').args} args
+     * @returns {Promise<void>}
+     */
+
+    execute = async (M) => {
+       const prefix = this.helper.config.prefix
+       const { bank, tag } = await this.client.DB.getUser(sender.jid)
+       const buttons = [
+        { buttonId: `${this.helper.config.prefix}wallet`, buttonText: {displayText: 'Wallet'}, type: 1},
+     ]
+       const buttonMessage = {
+            text: `🏦 *Bank* 🏦\n\n🧧 *Name:- ${sender.username}*\n\n  🌀 *Tag: #${tag}*\n\n🪙 *Gold: ${bank}*`,
+            footer: '© Eternity 2022',
+            buttons: buttons,
+            headerType: 1
+        }
+       return void (await this.client.sendMessage(M.from, buttonMessage))
+    }
+}
+
