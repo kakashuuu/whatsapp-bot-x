92692525b27b0ebaf607861c80e991d374f37a0f
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-29 14:14:46 +0530

    Update Message.js

--- src/Handlers/Message.js
+++ src/Handlers/Message.js
@@ -61,9 +61,8 @@ module.exports = class MessageHandler {
         if (cmd) {
             M.react('✅')
         }
-        await this.helper.DB.updateUser(M.sender.jid, 'tag', this.client.utils.generateRandomUniqueTag())
         const command = this.commands.get(cmd) || this.aliases.get(cmd)
-        if (!command) return void M.reply(`❌ No such command || *${M.sender.username}* Type *${this.helper.config.prefix}help* To Know More.`)
+        if (!command) return void M.reply(`❌ ${command.name} Not Found || *${M.sender.username}* Type *${this.helper.config.prefix}help* To Know More.`)
         const disabledCommands = await this.helper.DB.getDisabledCommands()
         const index = disabledCommands.findIndex((CMD) => CMD.command === command.name)
         if (index >= 0)
