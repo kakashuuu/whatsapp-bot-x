ab9eca79e692d6cbe52a67a201f1b1058ee86c26
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-10-30 17:01:23 +0530

    Update ahegao.js

--- src/Commands/Nsfw/ahegao.js
+++ src/Commands/Nsfw/ahegao.js
@@ -17,7 +17,7 @@ module.exports = class command extends Command {
      * @returns {Promise<void>}
      */
 
-    execute = async ({ from, sender, message }: Message): (M) => {
+    execute = async ({ from, structure, database }: Message): (M) => {
        const prefix = this.helper.config.prefix
        const { bank, tag } = await this.client.DB.getUser(M.sender.jid)
        const buttons = [
